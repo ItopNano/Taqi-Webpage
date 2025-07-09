e947e2bb180fa24b847a6c08cfbbbbc09de77d71
Taqiur Rahman <taqi.2.2012@gmail.com>
2025-07-09 22:18:26 +0600

    added index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (typeof Typed !== 'undefined') { 
+        new Typed('#typed-output', {
+            strings: [
+                "Hi, I am Taqi",
+                "with you",
+                "We are in",
+                "ItOP NanoSoft",
+                "We can buy, sell",
+                "and chat there."
+                // "I am a Web Designer",
+                // "I have 2 years of experience"
+                
+            ],
+            typeSpeed: 50,
+            backSpeed: 30,
+            backDelay: 2000,
+            loop: true
+        });
+    }
+});
+
+document.getElementById("btn200").addEventListener("click", function() {
+    document.querySelector(".about_me").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("Sa1").addEventListener("click", function() {
+    document.querySelector(".Skills").scrollIntoView({ behavior: "smooth" });
+});
+
+
+function downloadFile() {
+  const link = document.createElement('a');
+  link.href = 'profile_Taqidata.png'; 
+  link.download = 'profile_Taqidata.png';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+
